2ebd81bf0937b945a3e3d1423503f437329ea67e
Leonardo Zamoiski <leozamoiski@gmail.com>
2020-12-20 22:45:24 -0300

    starting project spring boot

---
+++ src/test/java/com/leonardo/workshopspring/WorkshopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
