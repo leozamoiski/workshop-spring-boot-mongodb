5b5375f956c25f95c41094eff63bbde8a3b51568
Leonardo Zamoiski <leozamoiski@gmail.com>
2020-12-25 11:20:18 -0300

    posts Endpoint added.

--- src/main/java/com/leonardo/workshopspring/config/Instantiation.java
+++ src/main/java/com/leonardo/workshopspring/config/Instantiation.java
@@ -19,32 +19,34 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		Post post1 = new Post(null, sdf.parse("25/12/2020"), "Partiu Viagem", "Vou viajar. Abraço!", new AuthorDTO (maria));
-		Post post2 = new Post(null,sdf.parse("24/12/2020"), "Bom dia", "ACORDEI AGORA!!", new AuthorDTO (maria));
-		
 
-		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("25/12/2020"), "Partiu Viagem", "Vou viajar. Abraço!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("24/12/2020"), "Bom dia", "ACORDEI AGORA!!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
+		
 	}
 
 }
